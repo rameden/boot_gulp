9652ab1139bddf5f1df1c0a126951be0c7e86094
Ryley Ameden <ryley@ameden.me>
2016-12-13 20:47:59 -0500

    ghpages deploy

--- gulpfile.js
+++ gulpfile.js
@@ -8,9 +8,10 @@ var reload      = browserSync.reload;
 var paths       = require('./gulp-tasks/paths.json');
 var files       = require('./gulp-tasks/files.json');
 var del         = require('del');
+var ghPages     = require('gulp-gh-pages');
 
 var getTask = function(task) {
-  return require('./gulp-tasks/' + task)(gulp, $, browserSync, reload, merge, paths, files, del);
+  return require('./gulp-tasks/' + task)(gulp, $, browserSync, reload, merge, paths, files, del, ghPages);
 };
 
 //clean
@@ -29,6 +30,9 @@ gulp.task('media', getTask('media'));
 gulp.task('svg', getTask('svg'));
 // SVG Sprite
 gulp.task('svg-sprite', getTask('svg-sprite'));
+// Deploy for GitHub Pages
+gulp.task('deploy', getTask('deploy'));
+
 
 // Bundled Tasks
 gulp.task('default', [
